5f53f3a9de08c18d2c783d8f5656676bf993c8a7
AreyBRSK <areybrsk@gmail.com>
2022-05-30 16:07:05 +0300

    Initial Commit

---
+++ TaskList_Lesson_2.13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaskList_Lesson_2.13
+//
+//  Created by Андрей Барсук on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
